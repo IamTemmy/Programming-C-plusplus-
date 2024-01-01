e713b068363780fcbd122bda531013d40f13822b
Temmy Adesola <143538331+IamTemmy@users.noreply.github.com>
2023-12-31 23:58:02 -0600

    Update practice (area of rectangle).cpp

--- Coding/Mensuration/practice (area of rectangle).cpp
+++ Coding/Mensuration/practice (area of rectangle).cpp
@@ -1,9 +1,14 @@
+//very basic code. Introductory level to finding the area of a rectangle
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+//length of shape is fixed and given as 4.3 is assigned to it
     double length = 4.3;
+//wigdth of shape is fixed and given as 3.2 is assigned to it
     double width = 3.2;
+//formula of area of rectangle which gives the answer by multiplying the assigned numbers to both variables
     cout<<length * width; 
 }
